6c34b21de0631d4645b7c6f3060188766d79bd20
Rodrigo Guido Arias <rodrigoguidoarias92@gmail.com>
2022-09-16 20:02:50 +0200

    first commit

---
+++ src/bkhosteldevs/Cliente.java
@@ -0,0 +1,20 @@
+
+package bkhosteldevs;
+
+/**
+ *
+ * @author rodri
+ */
+public class Cliente {
+
+    String nombre;
+    int telefono;
+    String email;
+
+    public Cliente(String nombre, int telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+}
---
+++ src/bkhosteldevs/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bkhosteldevs;
+
+import java.io.File;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author rodri
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        menu.setSize(400, 500);
+        menu.setLocationRelativeTo(null);
+        menu.setVisible(true);
+    }
+
+}
